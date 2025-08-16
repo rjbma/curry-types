14ff731d789040298328e327c735cc5a2b56ec54
rjbma <ricardo.araujo@obconnect.io>
2025-08-16 15:05:12 +0100

    chore: cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Task, TaskType } from './Task'
 import { Either, EitherType } from './Either'
+import { Utils } from './utils'
 
-export { Task, TaskType, Either, EitherType }
+export { Task, TaskType, Either, EitherType, Utils }
